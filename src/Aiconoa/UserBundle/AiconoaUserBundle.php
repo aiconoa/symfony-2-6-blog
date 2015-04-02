17b93610a683291e962075b9a2bdb24fde716855
Thomas Gros <thomas.gros@aiconoa.com>
2015-04-02 15:17:07 +0200

    user authentication from database

---
+++ src/Aiconoa/UserBundle/AiconoaUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Aiconoa\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AiconoaUserBundle extends Bundle
+{
+}
---
+++ src/Aiconoa/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Aiconoa\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+            'User/Default/login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+    }
+
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function loginCheckAction()
+    {
+        // this controller will not be executed,
+        // as the route is handled by the Security system
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction()
+    {
+    }
+}
